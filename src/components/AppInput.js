0e3398bf3b89b5c6a3029f6bba109a49003eb270
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-28 18:42:15 +0200

    small adjustments for re-usable components

--- src/components/AppInput.js
+++ src/components/AppInput.js
@@ -2,11 +2,13 @@ const AppInput = (props) => {
   return (
     <input
       className="flex-shrink w-full min-w-1 p-4 text-2xl text-slate-900 placeholder:text-slate-300 rounded-r-none rounded-l-sm border border-slate-300 outline-none"
-      type="number"
+      type={props.isBank ? "text" : "number"}
       value={props.value}
       placeholder={props.placeholder}
       required={props.required}
-      onChange={(e) => props.onChange(e.target.value)}
+      readOnly={props.readonly}
+      maxLength={props.isBank ? "10" : "524288"}
+      onChange={(e) => props.onChange && props.onChange(e.target.value)}
     />
   );
 };

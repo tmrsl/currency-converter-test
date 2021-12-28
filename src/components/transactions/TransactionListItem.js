991555e91e01be99014c13d7c9e059ff3ab92d64
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-28 18:40:44 +0200

    refactor, more components for re-usable stuff

--- src/components/transactions/TransactionListItem.js
+++ src/components/transactions/TransactionListItem.js
@@ -1,3 +1,5 @@
+import TransactionQtyAndCurrency from "./TransactionQtyAndCurrency";
+
 const TransactionListItem = ({
   sendQty,
   sendCurrency,
@@ -10,17 +12,13 @@ const TransactionListItem = ({
       className="grid grid-cols-[1fr_30px_1fr] justify-evenly px-4 py-5 first:border-t border-b border-slate-300"
       onClick={onClick}
     >
-      <span className="px-2">
-        <span className="mr-2 font-semibold text-blue-400">{sendCurrency}</span>
-        <span className="truncate font-light text-slate-800">{sendQty}</span>
-      </span>
+      <TransactionQtyAndCurrency isSend qty={sendQty} currency={sendCurrency} />
       <span> > </span>
-      <span className="px-2 overflow-hidden">
-        <span className="mr-2 font-semibold text-orange-400">
-          {receiveCurrency}
-        </span>
-        <span className="truncate font-light text-slate-800">{receiveQty}</span>
-      </span>
+      <TransactionQtyAndCurrency
+        side="receive"
+        qty={receiveQty}
+        currency={receiveCurrency}
+      />
     </div>
   );
 };
---
+++ src/components/transactions/TransactionQtyAndCurrency.js
@@ -0,0 +1,16 @@
+const TransactionQtyAndCurrency = ({ qty, currency, isSend }) => {
+  return (
+    <span className="px-2 overflow-hidden">
+      <span
+        className={
+          "mr-2 font-semibold " + (isSend ? "text-blue-400" : "text-orange-400")
+        }
+      >
+        {currency}
+      </span>
+      <span className="truncate font-light text-slate-800">{qty}</span>
+    </span>
+  );
+};
+
+export default TransactionQtyAndCurrency;

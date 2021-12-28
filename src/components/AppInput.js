49965529511a260abaf1978171a76422ae6d7337
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-28 14:58:40 +0200

    adjustments to reusable components

--- src/components/AppInput.js
+++ src/components/AppInput.js
@@ -1,10 +1,11 @@
 const AppInput = (props) => {
   return (
     <input
-      className="flex-shrink min-w-1 p-4 text-2xl text-slate-900 placeholder:text-slate-300 rounded-r-none rounded-l-sm border border-t-slate-300 outline-none"
+      className="flex-shrink w-full min-w-1 p-4 text-2xl text-slate-900 placeholder:text-slate-300 rounded-r-none rounded-l-sm border border-t-slate-300 outline-none"
       type="number"
       value={props.value}
       placeholder={props.placeholder}
+      required={props.required}
       onChange={(e) => props.onChange(e.target.value)}
     />
   );

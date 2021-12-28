48826f366b14a958e97fe37485265a04f66f2660
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-12-28 21:52:19 +0200

    minor updates

--- src/components/AppInput.js
+++ src/components/AppInput.js
@@ -2,7 +2,7 @@ const AppInput = (props) => {
   return (
     <input
       className="flex-shrink w-full min-w-1 p-4 text-2xl text-slate-900 placeholder:text-slate-300 rounded-r-none rounded-l-sm border border-slate-300 outline-none"
-      type={props.isBank ? "text" : "number"}
+      type="text"
       value={props.value}
       placeholder={props.placeholder}
       required={props.required}
